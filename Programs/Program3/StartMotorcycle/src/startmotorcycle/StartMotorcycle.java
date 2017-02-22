2e69cfc22011763ecd02c0828f2e0ff0619c5856
Bryce Henley <brycehenley97@gmail.com>
2017-02-21 20:35:07 -0700

    Swap Pairs

---
+++ Programs/Program3/StartMotorcycle/src/startmotorcycle/StartMotorcycle.java
@@ -0,0 +1,49 @@
+/*
+ * Bryce Henley
+ * CSC110AB
+ * 02/14/2017
+ * 
+ * The MIT License
+ *
+ * Copyright 2017 Bryce Henley.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package startmotorcycle;
+
+import java.util.Scanner;
+
+public class StartMotorcycle {
+    
+    public static int engineState;
+    
+    
+    public static void main(String[] args) {
+        
+        Scanner cli = new Scanner(System.in);
+       
+        System.out.println("It is cold outside and you must start your carborated motorcycle in standard procedure" + "\n");
+        
+        System.out.println("pull front brake (y/n)");
+        String brake = 
+        brake = cli.next();
+        
+    }
+    
+}
